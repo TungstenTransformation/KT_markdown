408425d22d4216845b17fb1625070fed5f1df3fa
David Wright <david.wright@kofax.com>
2021-03-01 21:51:44 +0100

    output fields

--- Program.vb
+++ Program.vb
@@ -9,17 +9,20 @@ Module Program
         Dim out As String = ""
         If args.Length = 0 Then Console.WriteLine("give path to fpr file") : Exit Sub
         If Not File.Exists(args(0)) Then
-            Console.WriteLine("Cannot find " + args(0)) : Exit Sub
+            Console.WriteLine("Cannot find " & args(0)) : Exit Sub
         End If
         XML = gz.gzip2xml(args(0))
-        out += "# KT Project auto documentation" + vbCrLf
-        out += "project version= " + XML.SelectSingleNode("/project").Attributes("version").InnerText + vbCrLf
+        out &= "# KT Project auto documentation" & vbCrLf
+        out &= "project version= " & XML.SelectSingleNode("/project").Attributes("version").InnerText & vbCrLf
         For Each cl As Xml.XmlNode In XML.SelectNodes("//class")
             Dim classname As String = cl.Attributes("name").InnerText
             If classname = "" Then classname = "project"
-            out += "# " + classname + vbCrLf
+            out &= "# Class: " & classname & vbCrLf
+            For Each field As Xml.XmlNode In cl.SelectNodes("field")
+                out &= " * " & field.Attributes("name").InnerText & ":" & vbCrLf
+            Next
             Dim script As String = cl.SelectSingleNode("script").InnerText
-            If script <> "" Then out += "script" + vbCrLf + "```vb" + vbCrLf + +"```" + vbCrLf
+            If script <> "" Then out &= "script" & vbCrLf & "```vb" & vbCrLf & script & "```" & vbCrLf
         Next
         System.Console.Write(out)
     End Sub
