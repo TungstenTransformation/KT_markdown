15491b3944c9f4db2ad16527dbc88cef9b35e117
David Wright <david.wright@kofax.com>
2021-03-03 17:10:53 +0100

    Moved xpaths to subclass and made separate class file

--- Program.vb
+++ Program.vb
@@ -4,36 +4,11 @@ Imports System.IO
 
 Module Program
     Sub Main(args As String())
-        Dim gz As New gzip
-        Dim XML As Xml.XmlDocument
-        Dim out As String = ""
         If args.Length = 0 Then Console.WriteLine("give path to fpr file") : Exit Sub
         If Not File.Exists(args(0)) Then
             Console.WriteLine("Cannot find " & args(0)) : Exit Sub
         End If
-        XML = gz.gzip2xml(args(0))
-        out &= "# KT Project auto documentation" & vbCrLf
-        out &= "project version= " & XML.SelectSingleNode("/project").Attributes("version").InnerText & vbCrLf
-        For Each cl As Xml.XmlNode In XML.SelectNodes("//class")
-            Dim classname As String = cl.Attributes("name").InnerText
-            If classname = "" Then classname = "project"
-            out &= "# Class: " & classname & vbCrLf
-            For Each field As Xml.XmlNode In cl.SelectNodes("field")
-                out &= " * field: " & field.Attributes("name").InnerText & ":" & vbCrLf
-            Next
-            For Each locator As Xml.XmlNode In cl.SelectNodes("locator")
-                out &= " * locator: " & locator.Attributes("name").InnerText & ":"
-                Try
-                    out &= locator.SelectSingleNode("extrname").InnerText
-                Catch ex As Exception
-
-                End Try
-                out &= vbCrLf
-            Next
-
-            Dim script As String = cl.SelectSingleNode("script").InnerText
-            If script <> "" Then out &= "script" & vbCrLf & "```vb" & vbCrLf & script & "```" & vbCrLf
-        Next
-        System.Console.Write(out)
+        Dim xdoc As New XDoc(args(0))
+        System.Console.Write(xdoc.Markdown)
     End Sub
 End Module
---
+++ XDoc.vb
@@ -0,0 +1,50 @@
+﻿Public Class XDoc
+    Private ReadOnly XML As Xml.XmlDocument
+    Sub New(filename As String)
+        Dim gz As New gzip
+        XML = gz.gzip2xml(filename)
+    End Sub
+    ReadOnly Property Markdown As String
+        Get
+            Markdown = "# KT Project auto documentation" & vbCrLf
+            Markdown &= "project version= " & XML.SelectSingleNode("/project").Attributes("version").InnerText & vbCrLf
+            Markdown &= Classes
+            Markdown &= Scripts
+            Markdown &= Properties
+        End Get
+    End Property
+    Private ReadOnly Property Scripts As String
+        Get
+            Throw New NotImplementedException
+        End Get
+    End Property
+    Private ReadOnly Property Properties As String
+        Get
+            Throw New NotImplementedException
+        End Get
+    End Property
+    Private ReadOnly Property Classes As String
+        Get
+            Classes = ""
+            For Each cl As Xml.XmlNode In XML.SelectNodes("//class")
+                Dim classname As String = cl.Attributes("name").InnerText
+                If classname = "" Then classname = "project"
+                Classes &= "# Class: " & classname & vbCrLf
+                For Each field As Xml.XmlNode In cl.SelectNodes("field")
+                    Classes &= " * field: " & field.Attributes("name").InnerText & ":" & vbCrLf
+                Next
+                For Each locator As Xml.XmlNode In cl.SelectNodes("locator")
+                    Classes &= " * locator: " & locator.Attributes("name").InnerText & ":"
+                    Try
+                        Classes &= locator.SelectSingleNode("extrname").InnerText
+                    Catch ex As Exception
+
+                    End Try
+                    Classes &= vbCrLf
+                Next
+                Dim script As String = cl.SelectSingleNode("script").InnerText
+                If script <> "" Then Classes &= "script" & vbCrLf & "```vb" & vbCrLf & script & "```" & vbCrLf
+            Next
+        End Get
+    End Property
+End Class
